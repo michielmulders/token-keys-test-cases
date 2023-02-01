a26d4e59b01fbd4711a10b59bfeebf06fba25af9
Michiel Mulders <michielmulders1@gmail.com>
2023-02-01 16:41:59 +0100

    Add example with all keys set for token
    Signed-off-by: Michiel Mulders <michielmulders1@gmail.com>

---
+++ all-keys-remove-supply.js
@@ -0,0 +1,93 @@
+console.clear();
+require("dotenv").config();
+
+const {
+  AccountId,
+  PrivateKey,
+  Client,
+  TokenCreateTransaction,
+  TokenInfoQuery,
+  TokenType,
+  CustomRoyaltyFee,
+  CustomFixedFee,
+  Hbar,
+  TokenSupplyType,
+  TokenMintTransaction,
+  TokenBurnTransaction,
+  TransferTransaction,
+  AccountBalanceQuery,
+  AccountUpdateTransaction,
+  TokenAssociateTransaction,
+  TokenNftInfoQuery,
+  NftId,
+  AccountCreateTransaction,
+} = require("@hashgraph/sdk");
+
+// Configure accounts and client, and generate needed keys
+const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+const operatorKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
+const client = Client.forTestnet().setOperator(operatorId, operatorKey);
+client.setDefaultMaxTransactionFee(new Hbar(100));
+
+const adminKey = PrivateKey.generateED25519();
+const randomKey = PrivateKey.generateED25519();
+const treasuryKey = PrivateKey.generateED25519();
+
+async function main() {
+  // Create accounts
+  console.log(`- Creating accounts...`);
+  const [adminAccStatus, adminId] = await accountCreatorFcn(adminKey, 5);
+  console.log(`- Created admin account ${adminId} that has a balance of 5ℏ`);
+
+  const [randomAccStatus, randomId] = await accountCreatorFcn(randomKey, 5);
+  console.log(`- Created random account ${randomId} that has a balance of 5ℏ`);
+  
+  const [treasuryAccStatus, treasuryId] = await accountCreatorFcn(treasuryKey, 5);
+  console.log(`- Created random account ${treasuryId} that has a balance of 5ℏ`);
+
+  // Create NFT
+  console.log(`\n- Creating NFT (with all token keys set)`);
+  let nftCreate = await new TokenCreateTransaction()
+    .setTokenName("Fall Collection")
+    .setTokenSymbol("LEAF")
+    .setTokenType(TokenType.NonFungibleUnique)
+    .setDecimals(0)
+    .setInitialSupply(0)
+    .setTreasuryAccountId(treasuryId)
+    .setSupplyType(TokenSupplyType.Finite)
+    .setMaxSupply(5)
+    // Set keys
+    .setAdminKey(adminKey)
+    .setFreezeKey(randomKey)
+    .setKycKey(randomKey)
+    .setWipeKey(randomKey)
+    .setSupplyKey(randomKey)
+    .setPauseKey(randomKey)
+    .setFeeScheduleKey(randomKey)
+    .freezeWith(client)
+    .sign(treasuryKey);
+
+  let nftCreateTxSign = await (await nftCreate.sign(adminKey)).sign(treasuryKey);
+  let nftCreateSubmit = await nftCreateTxSign.execute(client);
+  let nftCreateRx = await nftCreateSubmit.getReceipt(client);
+  let tokenId = nftCreateRx.tokenId;
+  console.log(`- Created NFT with Token ID: ${tokenId}`);
+
+  let tokenInfo = await new TokenInfoQuery().setTokenId(tokenId).execute(client);
+  console.log(`- Current NFT supply: ${tokenInfo.totalSupply}`);
+
+  client.close();
+
+
+  // ACCOUNT CREATOR FUNCTION ==========================================
+  async function accountCreatorFcn(pvKey, iBal) {
+    const response = await new AccountCreateTransaction()
+      .setInitialBalance(new Hbar(iBal))
+      .setKey(pvKey.publicKey)
+      .execute(client);
+    const receipt = await response.getReceipt(client);
+    return [receipt.status, receipt.accountId];
+  }
+}
+
+main();
